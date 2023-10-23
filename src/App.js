399f56abdab4796be5226544f72df8b6a7e86256
tahminaislamshammee <tahminaislamshammee@gmail.com>
2023-10-23 00:34:48 -0400

    changed browserrouter name and json file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './responsive.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Portfolio from './pages/portfolio/Portfolio';
@@ -10,7 +10,7 @@ import Themes from './Components/Theme';
 function App() {
   return (
 
-    <BrowserRouter basename='/personalizedPortfolioReact'>
+    <Router basename='/personalizedPortfolioReact'>
     <NavBar/>
     <Themes/>
       <Routes>
@@ -19,7 +19,7 @@ function App() {
         <Route path='portfolio' element={<Portfolio/>}></Route>
         <Route path='contact' element={<Contact/>}></Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
