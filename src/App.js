c1d613ef9d1a6acf1c8a70c7ac622dcb71ef3317
tahminaislamshammee <tahminaislamshammee@gmail.com>
2023-10-23 00:23:00 -0400

    added exact path for home page

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <NavBar/>
     <Themes/>
       <Routes>
-        <Route index element={<Home/>}></Route>
+        <Route exact path='/' element={<Home/>}></Route>
         <Route path='about' element={<About/>}></Route>
         <Route path='portfolio' element={<Portfolio/>}></Route>
         <Route path='contact' element={<Contact/>}></Route>
